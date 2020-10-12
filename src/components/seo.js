b11aa537cc33ef61b9237a49f799d8593ad5ecfd
k1r1m1 <kirimi@paygoenergy.co>
2020-10-12 23:06:22 +0300

    add babel-eslint as parser to fix unexpected token (.) in seo.js file

--- src/components/seo.js
+++ src/components/seo.js
@@ -3,9 +3,7 @@ import { graphql, useStaticQuery } from 'gatsby';
 import { Helmet } from 'react-helmet';
 
 function SEO({ title, seo }) {
-  const {
-    site: { siteMetadata },
-  } = useStaticQuery(graphql`
+  const { site: { siteMetadata } } = useStaticQuery(graphql`
     {
       site {
         siteMetadata {
@@ -19,9 +17,9 @@ function SEO({ title, seo }) {
 
   const defaultTitle = siteMetadata.title;
 
-  const pageDescription = seo.description || siteMetadata.description;
-  const pageKeywords = seo.keywords || siteMetadata.keywords;
-  const pageTitle = seo.title || title || 'Home';
+  const pageDescription = seo?.description || siteMetadata.description;
+  const pageKeywords = seo?.keywords || siteMetadata.keywords;
+  const pageTitle = seo?.title || title || 'Home';
 
   return (
     <Helmet
@@ -32,18 +30,18 @@ function SEO({ title, seo }) {
       <title>{pageTitle}</title>
       <meta name="description" content={pageDescription} />
       <meta name="keywords" content={pageKeywords} />
-      {seo.image && <meta property="image" content={seo.image.url} />}
+      {seo?.image && <meta property="image" content={seo.image.url} />}
 
       <meta property="og:title" content={pageTitle} />
       <meta property="og:description" content={pageDescription} />
       <meta property="og:site_name" content={defaultTitle} />
-      {seo.image && <meta property="og:image" content={seo.image.url} />}
+      {seo?.image && <meta property="og:image" content={seo.image.url} />}
       <meta name="og:type" content="website" />
 
       <meta name="twitter:site" content="@GraphCMS" />
       <meta name="twitter:title" content={`${pageTitle} | ${defaultTitle}`} />
       <meta name="twitter:card" content="summary_large_image" />
-      {seo.image && <meta name="twitter:image:src" content={seo.image.url} />}
+      {seo?.image && <meta name="twitter:image:src" content={seo.image.url} />}
     </Helmet>
   );
 }
